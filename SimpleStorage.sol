a0765f4169eb71e599313955d7500c72cdd5608a
BipinParadva <107022624+BipinParadva@users.noreply.github.com>
2022-09-15 13:13:00 +0530

    Update SimpleStorage.sol
    Practical 1 Simple Storage

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,68 +1,30 @@
-pragma solidity ^0.6.0;
+// SPDX-License-Identifier: MIT
 
- 
+pragma solidity ^0.6.0;
 
 contract SimpleStorage {
 
-    // uint256 favoriteNumber = 5;
+    uint256 favoriteNumber;
 
-    // bool favoriteBool = false;
-
-    // string favoriteString = "String";
-
-    // int256 favoriteInt = -5;
-
-    // bytes32 favoriteBytes = "cat";
-
-    // address favoriteAddress = 0x9e8323c2bB08f9170333199212b0f8823A860A2c;
-
- 
-
-    uint256 public favoriteNumber;
-
- 
-
-    struct People{
-
-        uint favoriteNumber;
-
+    // This is a comment!
+    struct People {
+        uint256 favoriteNumber;
         string name;
-
     }
-
- 
 
     People[] public people;
-
     mapping(string => uint256) public nameToFavoriteNumber;
 
- 
-
-    People public person = People({favoriteNumber : 1, name : "Bipin"});
-
- 
-
-    function store(uint256 _favoriteNumber) public{
-
+    function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
-
     }
-
-    function retrieve() public view returns(uint256)
-
-    {
-
+    
+    function retrieve() public view returns (uint256){
         return favoriteNumber;
-
     }
 
-
-    function addperson(string memory _name , uint256 _favoriteNumber) public {
-
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
         people.push(People(_favoriteNumber, _name));
-
         nameToFavoriteNumber[_name] = _favoriteNumber;
-
     }
-
 }
